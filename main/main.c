25085badb95e877ab32ea4576f7c5459ba4f2c7e
ilacftemp <ilachafin@gmail.com>
2024-04-18 20:06:30 +0000

    codigo do joystick adicionado

--- main/main.c
+++ main/main.c
@@ -5,13 +5,13 @@
 #include <task.h>
 #include <semphr.h>
 #include <queue.h>
-
 #include <string.h>
-
 #include "pico/stdlib.h"
 #include <stdio.h>
-
 #include "hc06.h"
+#include "hardware/adc.h"
+#include <math.h>
+#include <stdlib.h>
 
 const int BTN_Confirm = 0; // z or enter
 const int BTN_Cancel = 1; // x or shift
@@ -28,6 +28,11 @@ const int LED_Connected = 6; // bluetooth connected
 
 QueueHandle_t xQueueAdc;
 QueueHandle_t xQueueBTN;
+
+typedef struct adc {
+    int axis;
+    int val;
+} adc_t;
 
 void button_callback(uint gpio, uint32_t events) {
     int pressed_btn = 0;
@@ -47,6 +52,61 @@ void button_callback(uint gpio, uint32_t events) {
     xQueueSendFromISR(xQueueBTN, &pressed_btn, NULL);
 }
 
+void joy_x_task(void *p) {
+    adc_t data;
+    adc_init();
+
+    while (1) {
+        adc_gpio_init(JOY_X_PIN);
+        adc_select_input(0);
+        data.axis = 0;
+        int val = adc_read();
+        val -= 2048;
+        val /= 8;
+        if (abs(val) < DEAD_ZONE) {
+            val = 0;
+        }
+        data.val = val;
+        xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
+void joy_y_task(void *p) {
+    adc_t data;
+    adc_init();
+
+    while (1) {
+        adc_gpio_init(JOY_Y_PIN);
+        adc_select_input(1);
+        data.axis = 1;
+        int val = adc_read();
+        val -= 2048;
+        val /= 8;
+        if (abs(val) < DEAD_ZONE) {
+            val = 0;
+        }
+        data.val = val;
+        xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
+void uart_task(void *p) {
+    adc_t data;
+
+    while (1) {
+        xQueueReceive(xQueueAdc, &data, portMAX_DELAY);
+        int msb = data.val >> 8;
+        int lsb = data.val & 0xFF;
+
+        uart_putc_raw(uart0, data.axis);
+        uart_putc_raw(uart0, lsb);
+        uart_putc_raw(uart0, msb);
+        uart_putc_raw(uart0, -1);
+    }
+}
+
 void hc06_task(void *p) {
     uart_init(HC06_UART_ID, HC06_BAUD_RATE);
     gpio_set_function(HC06_TX_PIN, GPIO_FUNC_UART);
@@ -64,10 +124,15 @@ int main() {
 
     printf("Start bluetooth task\n");
 
+    uart_init(uart0, 115200);
+
     xQueueBTN = xQueueCreate(10, sizeof(int));
-    xQueueAdc = xQueueCreate(10, sizeof(int));
+    xQueueAdc = xQueueCreate(32, sizeof(adc_t));
 
     xTaskCreate(hc06_task, "UART_Task 1", 4096, NULL, 1, NULL);
+    xTaskCreate(joy_x_task, "x_task", 4096, NULL, 1, NULL);
+    xTaskCreate(joy_y_task, "y_task", 4096, NULL, 1, NULL);
+    xTaskCreate(uart_task, "uart_task", 4096, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
